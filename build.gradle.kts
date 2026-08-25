40c0263b0eb7e3284f58458f01ca27b9b8ec9c45
qsum99 <someshwarkumbhar87@gmail.com>
2026-08-26 00:28:41 +0530

    fix(build): update KSP to 2.3.11 for Kotlin 2.3.0 compat + fix sourceSets DSL error
    - KSP 2.3.11 matches Kotlin 2.3.0 compiler
    - Added android.disallowKotlinSourceSets=false to gradle.properties
    - BUILD SUCCESSFUL: all 37 tasks pass

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
     id("com.android.application") version "9.2.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
     id("com.android.test") version "9.2.1" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    id("com.google.devtools.ksp") version "2.3.11" apply false
 }
 
